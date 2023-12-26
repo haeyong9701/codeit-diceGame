16d4946220a16baf0a218b3ba85238cafbf14661
haeyong9701 <haeyong9701@gmail.com>
2023-12-27 08:53:23 +0900

    feat: 참조형 State 연습

--- src/App.js
+++ src/App.js
@@ -8,14 +8,20 @@ const random = (n) => {
 
 const App = () => {
   const [num, setNum] = useState(1);
+  const [sum, setSum] = useState(0);
+  const [gameHistory, setGameHistory] = useState([]);
 
   const handleRollClick = () => {
     const nextNum = random(6);
     setNum(nextNum);
+    setSum(sum + nextNum);
+    setGameHistory([...gameHistory, nextNum]);
   };
 
   const handleClearClick = () => {
     setNum(1);
+    setSum(0);
+    setGameHistory([]);
   };
 
   return (
@@ -24,7 +30,13 @@ const App = () => {
         <Button onClick={handleRollClick}>던지기</Button>
         <Button onClick={handleClearClick}>처음부터</Button>
       </div>
-      <Dice color="blue" num={num} />
+      <div>
+        <h2>나</h2>
+        <Dice color="blue" num={num} />
+        <h2>총점</h2>
+        <p>{sum}</p>
+        <p>{gameHistory.join(", ")}</p>
+      </div>
     </>
   );
 };
