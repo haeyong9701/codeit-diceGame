19b640210c700e1dd7c786e51f7ef053d171267c
haeyong9701 <haeyong9701@gmail.com>
2023-12-26 17:41:48 +0900

    feat: State 연습

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import { useState } from "react";
 import Dice from "./Dice";
+import Button from "./Button";
+
+const random = (n) => {
+  return Math.ceil(Math.random() * n);
+};
 
 const App = () => {
+  const [num, setNum] = useState(1);
+
+  const handleRollClick = () => {
+    const nextNum = random(6);
+    setNum(nextNum);
+  };
+
+  const handleClearClick = () => {
+    setNum(1);
+  };
+
   return (
     <>
-      <Dice color="blue" num={3} />
+      <div>
+        <Button onClick={handleRollClick}>던지기</Button>
+        <Button onClick={handleClearClick}>처음부터</Button>
+      </div>
+      <Dice color="blue" num={num} />
     </>
   );
 };
